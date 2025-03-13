aa74a9e088a9a2a236e2b778e8a171c165a93d0b
Alexandra <cimentarowa@gmail.com>
2025-03-13 22:34:21 +0200

    practice

---
+++ Programming-Fundamentals-with-JavaScript/01.Basic Syntax-Conditional-Statements-and-Loops-Lab/10.Numbers-from-M-to-N.js
@@ -0,0 +1,16 @@
+function numbersFromMToN(num1, num2){
+    while(num1 >= num2) {
+        console.log(num1);
+        num1--;
+    }
+}
+
+//numbersFromMToN(6,2);
+
+// function numbersFromMToN(num1, num2){
+//     for(let i=num1; i>= num2; i--) {
+//         console.log(i);
+//     }
+// }
+
+// numbersFromMToN(6,2);
---
+++ Programming-Fundamentals-with-JavaScript/01.Basic Syntax-Conditional-Statements-and-Loops-Lab/9.Numbers-from-N-to-1.js
@@ -0,0 +1,9 @@
+function numbersFromN(num){
+    while(num >=1){
+        console.log(num);
+        num--;
+    }
+}
+
+//numbersFromN(5);
+//numbersFromN(9);
