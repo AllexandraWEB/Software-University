a8fb203e75fade620b6a896f3fdb3306888d8d96
Alexandra <cimentarowa@gmail.com>
2025-06-04 23:16:20 +0300

    add: Homework

--- JavaScript-Advanced/08.DOM-Introduction-Exercise/01. Subtraction/subtract.js
+++ JavaScript-Advanced/08.DOM-Introduction-Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumberRef = document.getElementById("firstNumber").value;
+    const secondNumberRef = document.getElementById("secondNumber").value;
+    const resultRef = document.getElementById("result"); // We take the reference to the DOM Element
+
+    let firstNumber = Number(firstNumberRef);
+    let secondNumber = Number(secondNumberRef);
+
+    let result = firstNumber - secondNumber; // Subtract the second number from the first one
+    resultRef.textContent = result;
 }
--- JavaScript-Advanced/08.DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ JavaScript-Advanced/08.DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-  //TODO...
+  const textRef = document.getElementById("text").value;
+  const namingConventionRef = document.getElementById("naming-convention").value;
+  const resultRef = document.getElementById("result");
+
+  let result;
+
+  if (namingConventionRef === "Camel Case") {
+    result = toCamelCase(textRef);
+  } else if (namingConventionRef === "Pascal Case") {
+    result = toPascalCase(textRef);
+  } else {
+    result = "Error!";
+  }
+
+  resultRef.textContent = result;
+
+  function toCamelCase(textRef) {
+    const words = textRef.toLowerCase().split(" ");
+    return words
+    .map((word, index) => {
+      if (index === 0) return word;
+      return capitalize(word);
+    })
+    .join("");
+  }
+
+  function toPascalCase(textRef) {
+    const words = textRef.toLowerCase().split(" ");
+    return words
+    .map(word => capitalize(word))
+    .join("");
+  }
+
+  function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
 }
