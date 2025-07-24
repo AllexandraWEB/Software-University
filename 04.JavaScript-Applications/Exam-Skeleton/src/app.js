42573ca982b958cb09114257fef45e2cc3016539
Alexandra <cimentarowa@gmail.com>
2025-07-24 15:49:49 +0300

    add: Test

---
+++ 04.JavaScript-Applications/Exam-Skeleton/src/app.js
@@ -0,0 +1,5 @@
+import * as api from "./data/request.js"
+import * as userApi from "./data/user.js"
+
+window.api = api;
+window.userApi = userApi;
---
+++ 04.JavaScript-Applications/Exam-Skeleton/src/data/user.js
@@ -0,0 +1,37 @@
+import { clearUserData, setUserData } from "../utils/utils.js";
+import { post } from "./request.js";
+
+const endpoints = {
+    login: "/users/login",
+    register: "/users/register",
+    logout: "/users/logout"
+}
+
+// TODO: Change the indentity based on the exam description
+export async function login(email, password) { // Instead of email, it could be username instead
+    const result = await post(endpoints.login, { email, password });
+
+    const userData = {
+        id: result._id,
+        accessToken: result.accessToken
+    };
+
+    setUserData(userData);
+}
+
+// TODO: Change the indentity based on the exam description
+export async function register(email, password) {
+    const result = await post(endpoints.register, { email, password });
+
+    const userData = {
+        id: result._id,
+        accessToken: result.accessToken
+    };
+
+    setUserData(userData);
+}
+
+export function logout() {
+    get(endpoints.logout);
+    clearUserData();
+}
