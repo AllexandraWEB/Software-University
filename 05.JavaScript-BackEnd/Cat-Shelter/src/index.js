c52a8aac46ecb7af8909668ab863d9f9ff4a60ce
Alexandra Tsimentarova <alexandratsimentarova@192.168.1.8>
2025-09-24 18:13:24 +0300

    add: Fix feature

--- 05.JavaScript-BackEnd/Cat-Shelter/src/index.js
+++ 05.JavaScript-BackEnd/Cat-Shelter/src/index.js
@@ -1,11 +1,15 @@
 import http from "http";
-import homeHtml from "./home.html.js"
-import siteCss from "./site.css.js"
+import fs from "fs/promises";
+import siteCss from "./site.css.js";
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/") {
+    const homeHtml = await fs.readFile("./src/views/home/index.html", {
+      encoding: "utf-8",
+    });
+
     res.writeHead(200, {
       "content-type": "text/html",
     });
