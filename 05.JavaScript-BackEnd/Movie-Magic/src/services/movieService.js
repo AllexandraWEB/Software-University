04bc6c688636755ef987fab2aefa3e9ff35c4f51
Alexandra Tsimentarova <cimentarowa@gmail.com>
2025-10-13 10:04:30 +0300

    save: Keep the latest updates

--- 05.JavaScript-BackEnd/Movie-Magic/src/services/movieService.js
+++ 05.JavaScript-BackEnd/Movie-Magic/src/services/movieService.js
@@ -1,17 +1,36 @@
 import Movie from "../models/Movie.js";
 
 export default {
-    async getAll(filter) {
+     getAll(filter) {
         // First way to solve the issue with handlebars - used .lean()
         // const result = await Movie.find(filter).lean();
 
         // Second way to solve the issue with handlebars - added allowProtoMethodsByDefault and allowProtoPropertiesByDefault
-        const result = await Movie.find(filter);
+        let query = Movie.find();
 
-        return result;
+        if (filter.title) {
+            // TODO Search by title, partial match, case insensitive
+            query = query.filter(movie => movie.title.toLowerCase().includes(filter.title.toLowerCase()))
+        }
+
+        if (filter.genre) {
+            // TODO Search by genre, exact match, case insensitive
+            query = query.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+        }
+
+        if (filter.year) {
+            query = query.where("year").equals(filter.year) // Mongoose variant
+            // MongoDB variant
+            // result = result.find({year: filter.year})
+        }
+
+        return query;
     },
     getOne(movieId) {
-        return Movie.findOne({_id: movieId});
+        // return Movie.findOne({_id: movieId});
+
+        // Use this instead
+        return Movie.findById(movieId);
     },
     create(movieData) {
         movieData.rating = Number(movieData.rating);
